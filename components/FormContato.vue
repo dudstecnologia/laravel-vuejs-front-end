a526feb582f4ff54d20cfb5c8eb47579f44f674a
Top Cine <topcine@protonmail.com>
2020-05-10 18:19:36 -0400

    Formulário, inicial

--- components/FormContato.vue
+++ components/FormContato.vue
@@ -1,11 +1,42 @@
 <template>
-  <p>Formulário de Contato</p>
+  <b-form class="mt-3" inline @submit="onSubmit" @reset="onReset">
+    <b-input id="nome" v-model="form.nome" class="mb-2 mr-2" placeholder="Nome *" required />
+    <b-input id="telefone" v-model="form.telefone" class="mb-2 mr-2" placeholder="Telefone *" required />
+    <b-input id="idade" v-model="form.idade" class="mb-2 mr-2" placeholder="Idade *" required />
+
+    <b-button class="mr-2" type="submit" variant="info">
+      Salvar
+    </b-button>
+    <b-button type="reset" variant="danger">
+      Limpar
+    </b-button>
+  </b-form>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      form: {
+        nome: '',
+        telefone: '',
+        idade: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset (evt) {
+      evt.preventDefault()
+      this.form = {
+        nome: '',
+        telefone: '',
+        idade: ''
+      }
+    }
   }
 }
 </script>
