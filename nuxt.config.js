467230e2bae2a4a04b276b8eeaedbcee899ebe5d
Eduardo Silva <eduardo@dudstecnologia.com.br>
2020-05-10 20:58:12 -0400

    Base URL Axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/axios'
   ],
   /*
   ** Nuxt.js dev-modules
---
+++ plugins/axios.js
@@ -0,0 +1,3 @@
+export default function ({ $axios }) {
+  $axios.setBaseURL('http://127.0.0.1:8000/api')
+}
